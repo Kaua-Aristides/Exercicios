f2d96ca52e959b6668763223650c9afdfa5b71d3
Kaua-Aristides <kauaaristide@gmail.com>
2025-09-17 15:51:17 -0300

    Criação do exercicio de calcular as notas

---
+++ Basico/CalcularNota/CalcularNota/CalcularNota/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularNota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variaveis
+            double nota1, nota2, resultado = 0;
+
+            //Entrada de dados
+            Console.WriteLine("Digite sua nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite sua nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            //Calculo dos dados
+            resultado = ((nota1 + nota2) / 2);
+
+            //Saida de dados
+            Console.WriteLine("Sua nota e: " + resultado);
+        }
+    }
+}
